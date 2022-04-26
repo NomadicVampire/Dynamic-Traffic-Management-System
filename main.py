75afdd8b04c3e44922741804a9eb4c32d65be90b
NomadicVampire <54809601+NomadicVampire@users.noreply.github.com>
2022-04-27 02:27:02 +0530

    Added Final code
    Added final code for which gives us waiting time for all lanes and gives the time to traffic signals dynamically

--- main.py
+++ main.py
@@ -7,19 +7,42 @@ images_folder = glob.glob("img/*.jpeg")
 
 vd = VehicleDetector()
 # lane arrays
-lane = []*4
+lane = []
 count = 0
+
+def syncLane(lane):
+    baseTimer = 20 # baseTimer = int(input("Enter the base timer value"))
+    timeLimits = [2, 5] # timeLimits = list(map(int,input("Enter the time limits ").split()))
+    # print("no of vehicles : ", *lane)
+
+    t = [(i / sum(lane)) * baseTimer if timeLimits[0] < (i / sum(lane)) * baseTimer < timeLimits[1] else min(timeLimits, key=lambda x: abs(x - (i / sum(lane)) * baseTimer)) for i in lane]
+    print("--------------------------")
+    print("Lane 1 waiting time :", t[0])
+    print("Lane 2 waiting time :", t[1])
+    print("Lane 3 waiting time :", t[2])
+    print("Lane 4 waiting time :", t[3])
+    print("Total average waiting time :", sum(t))
+
+    print("--------------------------")
+    w = int(sum(t)) *1000
+
+    return w
+
 
 # Loop through all the images
 for img_path in images_folder:
-    #     print("Img path", img_path)
+
+    # calls function to find waiting time for each lane when we get images of all 4 lanes
+    if (count % 4 == 0 and count != 0):
+        waitTime = syncLane(lane)
+        cv2.waitKey(waitTime)
+        lane.clear()
+        count = 0
+
+
     img = cv2.imread(img_path)
     vehicle_boxes = vd.detect_vehicles(img)
     vehicle_count = len(vehicle_boxes)
-
-    # ClassIndex, confidece, bbox = model.detect(img, confThreshold=0.5)
-    font_scale = 3
-    font = cv2.FONT_HERSHEY_PLAIN
 
     for box in vehicle_boxes:
         x, y, w, h = box
@@ -28,8 +51,15 @@ for img_path in images_folder:
     print("Total current count", vehicle_count)
     lane.append(vehicle_count)
     cv2.imshow("Cars", img)
-    cv2.waitKey(0)
+    count = count + 1
+    
+    cv2.waitKey(1000)
+
+# For last set of lane
+waitTime = syncLane(lane)
 
 
-for it in lane:
-    print(it)
+# total no. of vehicles for each lane
+# for it in lane:
+#     print(it)
+
